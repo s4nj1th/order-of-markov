e3443a3f4468df53bc60e3b94cd73babc060c267
sanjith <sanjith@tutamail.com>
2025-11-06 21:19:23 +0530

    updated: responsive images

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,38 +5,41 @@ import Analyzer from "./Analyzer";
 
 export default function MarkovOrderMVP(): React.ReactElement {
   return (
-    <div className="min-h-screen bg-background text-text relative overflow-x-hidden">
-      <img
-        src="/logo.png"
-        alt="logo"
-        className="w-40 absolute translate-x-10 md:translate-y-0 -translate-y-40"
-      />
-      <img
-        src="/ccru.gif"
-        alt="logo"
-        className="w-50 absolute right-0 bottom-40 -translate-x-10 filter dark:invert invert-0 hidden md:block"
-      />
-      <div className="max-w-4xl mx-auto px-4 pb-12 flex flex-col">
-        <div className="min-h-screen">
-          <div className="flex flex-col items-center mb-12">
-            <h1 className="text-4xl font-bold text-slate-800 mt-10">
-              Order of Markov
-            </h1>
-          </div>
+    <div className="min-h-screen bg-background text-text relative overflow-x-hidden flex flex-row items-center justify-center">
+      <div className="hidden sm:block w-1/5 max-w-[150px] items-start absolute top-0 left-5">
+        <img src="/logo.png" alt="logo" className="w-full h-auto" />
+      </div>
 
+      <main className="w-full sm:w-3/5 max-w-4xl px-4 py-10 items-center">
+        <header className="flex flex-col items-center mb-8">
+          <h1 className="text-4xl font-bold text-slate-800 mt-4">
+            Order of Markov
+          </h1>
+        </header>
+
+        <div className="mx-auto">
           <Analyzer />
         </div>
 
-        <div className="mt-12 text-center text-slate-500 text-sm border-t-1 border-slate-200 pt-4">
+        <footer className="mt-12 text-center text-slate-500 text-sm">
           <p>
             <a
               href="https://github.com/s4nj1th/order-of-markov"
               target="_blank"
+              rel="noreferrer"
             >
               Source Code
             </a>
           </p>
-        </div>
+        </footer>
+      </main>
+
+      <div className="hidden sm:block w-1/5 max-w-[150px] items-end absolute bottom-0 right-5">
+        <img
+          src="/ccru.gif"
+          alt="logo"
+          className="w-full h-auto filter invert"
+        />
       </div>
     </div>
   );
